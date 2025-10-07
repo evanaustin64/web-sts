fc37cbd76a463d07a673d1ccce476c3699bcae10
evanaustin64 <evanaustin64@gmail.com>
2025-10-07 15:31:16 +0700

    revisi 4

--- app/maintenance/layout.tsx
+++ app/maintenance/layout.tsx
@@ -1,14 +1,11 @@
 // app/maintenance/layout.tsx
+
 export default function MaintenanceLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang="id">
-      <body>
-        {children}
-      </body>
-    </html>
-  );
+  // Cukup kembalikan children. Boleh menggunakan React Fragment (<>) 
+  // atau biarkan seperti ini saja.
+  return <>{children}</>;
 }
